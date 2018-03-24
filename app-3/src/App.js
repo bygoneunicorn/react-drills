33f43bdf6eb866e75b542b4eafdbe6480ddc7d8d
bygoneunicorn <nathansoccer.jones@gmail.com>
2018-03-24 14:34:21 -0600

    practice-1 initial commit completed app questions 1-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      filterStr: '',
+      foods: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    }
+  }
+  foodFilter( filter ){
+    this.setState({filterStr: filter})
+  }
+
   render() {
+    var foodsDisplayed = this.state.foods.filter( (e,i) => {
+      return e.includes (this.state.filterStr);
+    }).map((e, i) =>{
+      return <h2 key={i}> {e} </h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" onChange = {(e)=> this.foodFilter(e.target.value)}/>
+        {foodsDisplayed}
       </div>
     );
   }
